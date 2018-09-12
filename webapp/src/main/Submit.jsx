d8d58959af5b387da488d2ded8aa783ba8a22c81
Andrew-Freitas-1106 <Chi.Zhang@academytrainee.com>
2018-09-12 13:05:58 +0100

    Reverting to Start state

--- webapp/src/main/Submit.jsx
+++
@@ -1,9 +0,0 @@
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-//
-// const Submit= () => {
-//     return (
-//         <Link to = ""> Submit </Link>
-//     )
-// }
-// export default Submit
